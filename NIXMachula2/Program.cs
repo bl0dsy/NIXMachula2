72f0957742d7cfdc0cae01a4a739a0118d0143c0
juliatkachenko99 <juliatkachenko99@gmail.com>
2021-11-10 02:47:36 +0200

    Add project files.

---
+++ NIXMachula2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NIXMachula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Узнайте стоимость пирожков в гривнах и копейках в зависимости от их количества");
+            int gryven, kopeek, quantity, quantity1, quantity2;
+            Console.WriteLine("Введите стоимость одного пирожка в гривнах");
+            gryven = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите стоимость одного пирожка в копейках");
+            kopeek = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество пирожков");
+            quantity = int.Parse(Console.ReadLine());
+            quantity1 = (gryven * quantity);
+            quantity2 =  (kopeek * quantity);
+            Console.WriteLine($"Стоимость в гривнах = { quantity1}");
+            Console.WriteLine($"Стоимость в копейках = { quantity2}");
+        }
+    }
+}
